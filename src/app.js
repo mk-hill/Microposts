e88b56f701d9bf3375699b7f80283bc6206aaa75
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-05 14:51:52 -0500

    Get and display posts from json server on dom load

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-let a;
+import { http } from './http';
+import { ui } from './ui';
+
+
+const getPosts = () => {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+};
+
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
