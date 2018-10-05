f9b7fe75861755f091859d3d0618dee56058fcf5
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-05 13:48:15 -0500

    Create html interface

--- src/app.js
+++ src/app.js
@@ -1,17 +1,1 @@
-import { testObj, moreTests } from './test';
-
-let test = () => console.log('test2');
-test();
-const props = testObj.func();
-
-async function getPosts() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-
-  const data = await response.json();
-  return data;
-}
-
-getPosts().then((posts) => {
-  console.log(posts);
-  alert(`${props} and ${moreTests}`);
-});
+let a;
--- src/test.js
+++
@@ -1,9 +0,0 @@
-export const testObj = {
-  key: 'prop',
-  key2: 'prop2',
-  func() {
-    return `${this.key}, ${this.key2}`;
-  }
-};
-
-export const moreTests = 59;
